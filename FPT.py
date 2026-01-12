ac2aa9c676008ec663bf83abbe8fd2b02ba144dc
MrRichardsHDSB <91692917+MrRichardsHDSB@users.noreply.github.com>
2026-01-11 23:46:55 -0500

    initial setup

---
+++ FPT.py
@@ -0,0 +1,71 @@
+'''
+-----------------------------------------------------------------------------
+Program Name: (never put your personal name or information on the Internet)
+Program Description:
+
+-----------------------------------------------------------------------------
+References:
+
+(put a link to your reference here but also add a comment in the code below where you used the reference)
+
+-----------------------------------------------------------------------------
+
+Additional Libraries/Extensions:
+
+(put a list of required extensions so that the user knows that they need to download extra features)
+
+-----------------------------------------------------------------------------
+
+Known bugs:
+
+(put a list of known bugs here, if you have any)
+
+----------------------------------------------------------------------------
+
+
+Program Reflection:
+I think this project deserves a level XXXXXX because ...
+
+ Level 3 Requirements Met:
+• 
+•  
+•  
+•  
+•  
+• 
+
+Features Added Beyond Level 3 Requirements:
+• 
+•  
+•  
+•  
+•  
+• 
+-----------------------------------------------------------------------------
+'''
+
+
+
+# *********SETUP**********
+
+
+
+# *********GAME LOOP**********
+
+
+
+
+    # *********EVENTS**********
+
+   
+    #PUT YOUR MOUSE/KEYBOARD EVENTS HERE
+   
+    # *********GAME LOGIC**********
+   
+    #PUT YOUR GAME LOGIN HERE FOR EACH GAMESTATE
+   
+    # *********DRAW THE FRAME**********
+
+    #PUT YOUR DRAWING, IMAGE PLACEMENT, BLIT ETC.. COMMANDS HERE FOR EACH GAMESTATE'''
+
+    # *********SHOW THE FRAME TO THE USER**********
