a06f48b17abc796ca31457bf760fd284183005cd
Vritss <vritika925@gmail.com>
2026-01-16 11:32:28 -0500

    Update FPT.py

--- FPT.py
+++ FPT.py
@@ -1,98 +1,135 @@
-'''
------------------------------------------------------------------------------
-Program Name: (never put your personal name or information on the Internet)
-Program Description:
+############################################
+# Ninja Frog Jump Game
+# Name: Vritika
+# Course: ICS
+# Date: Jan 2026
+#
+# Simple pygame jumping game
+import pygame
+import random
 
------------------------------------------------------------------------------
-References:
+# SETUP 
+pygame.init()
+WIDTH = 1000
+HEIGHT = 667
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Ninja Frog")
 
-(put a link to your reference here but also add a comment in the code below where you used the reference)
+clock = pygame.time.Clock()
 
------------------------------------------------------------------------------
+#LOAD IMAGES 
+frog_img = pygame.image.load("run01.png")
+frog_img = pygame.transform.scale(frog_img, (96, 96))  # resize frog
 
-Additional Libraries/Extensions:
+saw_img = pygame.image.load("saw01.png")
 
-(put a list of required extensions so that the user knows that they need to download extra features)
+# GAME STATES 
+MENU = 0
+PLAY = 1
+GAME_OVER = 2
+game_state = MENU
 
------------------------------------------------------------------------------
+# FROG VARIABLES 
+frog_x = 150
+frog_y = 450
+frog_y_speed = 0
+gravity = 1
+jump_power = -15
 
-Known bugs:
+#  SAW VARIABLES 
+saw_x = WIDTH
+saw_y = 500
+saw_size = random.randint(32, 50)
+saw_speed = 5
 
-(put a list of known bugs here, if you have any)
+# SCORE 
+score = 0
+font = pygame.font.SysFont(None, 36)
 
-----------------------------------------------------------------------------
+#  FUNCTIONS 
+def reset_saw():
+    """Resets saw position and size"""
+    global saw_x, saw_size
+    saw_x = WIDTH + random.randint(100, 300)
+    saw_size = random.randint(32, 50)
 
+#  MAIN LOOP
+running = True
+while running:
+    clock.tick(60)
 
-Program Reflection:
-I think this project deserves a level XXXXXX because ...
+    # EVENTS 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
- Level 3 Requirements Met:
-• 
-•  
-•  
-•  
-•  
-• 
+        # Mouse click for menu & restart
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if game_state == MENU:
+                game_state = PLAY
+                score = 0
+                reset_saw()
+            elif game_state == GAME_OVER:
+                game_state = MENU
 
-Features Added Beyond Level 3 Requirements:
-• 
-•  
-•  
-•  
-•  
-• 
------------------------------------------------------------------------------
-'''
-import pygame, random
+        # Jump when space is pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and game_state == PLAY:
+                if frog_y >= 450:
+                    frog_y_speed = jump_power
 
-BUG_FILES=["do this later]
-FONT_FILE="minecraft"
-SPLAT_FILE="buzz"
-W,H=1000,667; TARGET=15
+    # DRAW BACKGROUND 
+    screen.fill((200, 200, 200))  # simple background
 
-pygame.init
-try: pygame.mixer.init()
-except: pass
-screen=pygame.display.set_mode((1000,667))
-clock=pygame.time.Clock()
+    # MENU 
+    if game_state == MENU:
+        title = font.render("NINJA FROG", True, (0, 0, 0))
+        instructions = font.render("Click to Start | Space to Jump", True, (0, 0, 0))
+        screen.blit(title, (WIDTH // 2 - 80, HEIGHT // 2 - 40))
+        screen.blit(instructions, (WIDTH // 2 - 160, HEIGHT // 2 + 10))
 
-# Safe Loaders
-def img(f):
-    try: return pygame.image.load(f).convert_alpha()
-    except: return None
-def snd(f):
-    try: return pygame.mixer.Sound(f)
-    except: return None
+    # GAME 
+    if game_state == PLAY:
 
-def fnt(f,s):
-    try: return pygame.font.Font(f,s)
-    except: return pygame.font.SysFont(None,s)
+        # Gravity effect
+        frog_y_speed += gravity
+        frog_y += frog_y_speed
 
-# load the images
-bugs_i=[]
-for x in BUG_FILES:
-    i=img(x)
-    if i: bugs_i.append(i)
-    
-font=fnt(FONT_FILE,28)
-splat=snd(SPLAT_FILE)
+        # Stop frog on ground
+        if frog_y > 450:
+            frog_y = 450
+            frog_y_speed = 0
 
-START,PLAY,OVER="START","PLAY","OVER" # GAME STATES used to control screens
-state=START; score=0
+        # Move saw
+        saw_x -= saw_speed
 
-#The class
-class Bug:
-    def_init_(self):self.respawn()
-    
-    def respawn(self):
-        s=random.randint(75,100)
-    self.r=pygame.Rect(
-        random.randint(0,W-s) # X spawn range
-        random.randint(-250,-50), #change to (0,H-s) to spawn anywhere
-        s,s)
-    self.v=random.randint(2,6)
-    self.p=pygame.transform.scale(
-        random.choice(bugs_i),(s,s)) if bugs_i else None
-    
+        # Reset saw when off screen
+        if saw_x < -50:
+            reset_saw()
+            score += 1
 
+        # Collision detection
+        frog_rect = pygame.Rect(frog_x, frog_y, 96, 96)
+        saw_rect = pygame.Rect(saw_x, saw_y, saw_size, saw_size)
 
+        if frog_rect.colliderect(saw_rect):
+            game_state = GAME_OVER
+
+        # Draw frog and saw
+        screen.blit(frog_img, (frog_x, frog_y))
+        screen.blit(pygame.transform.scale(saw_img, (saw_size, saw_size)), (saw_x, saw_y))
+
+        # Draw score
+        score_text = font.render("Score: " + str(score), True, (0, 0, 0))
+        screen.blit(score_text, (20, 20))
+
+    # GAME OVER 
+    if game_state == GAME_OVER:
+        over_text = font.render("GAME OVER", True, (0, 0, 0))
+        restart_text = font.render("Click to return to menu", True, (0, 0, 0))
+        screen.blit(over_text, (WIDTH // 2 - 80, HEIGHT // 2 - 30))
+        screen.blit(restart_text, (WIDTH // 2 - 160, HEIGHT // 2 + 20))
+
+    pygame.display.update()
+
+pygame.quit()
