e6a86f72d14f8a8deafd184b86cd6a0da1096d12
Vritss <vritika925@gmail.com>
2026-01-16 10:43:46 -0500

    Update FPT.py

--- FPT.py
+++ FPT.py
@@ -43,29 +43,56 @@ Features Added Beyond Level 3 Requirements:
 • 
 -----------------------------------------------------------------------------
 '''
+import pygame, random
+
+BUG_FILES=["do this later]
+FONT_FILE="minecraft"
+SPLAT_FILE="buzz"
+W,H=1000,667; TARGET=15
+
+pygame.init
+try: pygame.mixer.init()
+except: pass
+screen=pygame.display.set_mode((1000,667))
+clock=pygame.time.Clock()
+
+# Safe Loaders
+def img(f):
+    try: return pygame.image.load(f).convert_alpha()
+    except: return None
+def snd(f):
+    try: return pygame.mixer.Sound(f)
+    except: return None
+
+def fnt(f,s):
+    try: return pygame.font.Font(f,s)
+    except: return pygame.font.SysFont(None,s)
+
+# load the images
+bugs_i=[]
+for x in BUG_FILES:
+    i=img(x)
+    if i: bugs_i.append(i)
+    
+font=fnt(FONT_FILE,28)
+splat=snd(SPLAT_FILE)
+
+START,PLAY,OVER="START","PLAY","OVER" # GAME STATES used to control screens
+state=START; score=0
+
+#The class
+class Bug:
+    def_init_(self):self.respawn()
+    
+    def respawn(self):
+        s=random.randint(75,100)
+    self.r=pygame.Rect(
+        random.randint(0,W-s) # X spawn range
+        random.randint(-250,-50), #change to (0,H-s) to spawn anywhere
+        s,s)
+    self.v=random.randint(2,6)
+    self.p=pygame.transform.scale(
+        random.choice(bugs_i),(s,s)) if bugs_i else None
+    
 
 
-
-# *********SETUP**********
-
-
-
-# *********GAME LOOP**********
-
-
-
-
-    # *********EVENTS**********
-
-   
-    #PUT YOUR MOUSE/KEYBOARD EVENTS HERE
-   
-    # *********GAME LOGIC**********
-   
-    #PUT YOUR GAME LOGIN HERE FOR EACH GAMESTATE
-   
-    # *********DRAW THE FRAME**********
-
-    #PUT YOUR DRAWING, IMAGE PLACEMENT, BLIT ETC.. COMMANDS HERE FOR EACH GAMESTATE'''
-
-    # *********SHOW THE FRAME TO THE USER**********
